889de2cd8e72a2b7fb5031bd741a243df4b6b4c3
Adam <adam.mhatre@menloschool.org>
2020-09-02 15:13:01 -0700

    number of ways to make change

---
+++ change.java
@@ -0,0 +1,23 @@
+public class change 
+{
+		public static void main(String[] args) 
+		{
+				System.out.println("Hello World");
+		}
+}
+
+		public static int NumberOfWaysToMakeChange(int cents)
+		{
+			if(cents == 0)
+			{
+				return 1;
+			}
+
+			int ways = 0;
+
+			for(int i = 0; i < cents; i++)
+			{
+				if(i % 25 != 0)
+					i
+			}
+		}
